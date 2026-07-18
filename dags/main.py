4d7964063e787e5321a3f828ce6961822c0a16e9
mehdielhadeg <mehdi.elhadeg2020@gmail.com>
2026-07-18 18:06:46 +0000

    added postgres datawarehouse

--- dags/main.py
+++ dags/main.py
@@ -1,7 +1,7 @@
 from airflow import DAG
 import pendulum
 from datetime import datetime, timedelta
-
+from airflow.operators.trigger_dagrun import TriggerDagRunOperator
 
 from api.video_stats import (
     get_playlist_id,
@@ -9,6 +9,8 @@ from api.video_stats import (
     extract_video_data,
     save_to_json,
 )
+
+from datawarehouse.dwh import staging_table, core_table
 
 # Define the local timezone
 local_tz = pendulum.timezone("Africa/Casablanca")
@@ -28,14 +30,18 @@ default_args = {
     # 'end_date': datetime(2030, 12, 31, tzinfo=local_tz),
 }
 
+# Variables
+staging_schema = "staging"
+core_schema = "core"
 
+# DAG 1: produce_json
 with DAG(
     dag_id="produce_json",
     default_args=default_args,
     description="DAG to produce JSON file with raw data",
     schedule="0 14 * * *",
     catchup=False,
-) as dag :
+) as dag_produce:
 
     # Define tasks
     playlist_id = get_playlist_id()
@@ -43,5 +49,29 @@ with DAG(
     extract_data = extract_video_data(video_ids)
     save_to_json_task = save_to_json(extract_data)
 
+    trigger_update_db = TriggerDagRunOperator(
+        task_id="trigger_update_db",
+        trigger_dag_id="update_db",
+       wait_for_completion=True,
+       poke_interval=30,
+    )
+
     # Define dependencies
-    playlist_id >> video_ids >> extract_data >> save_to_json_task
+    playlist_id >> video_ids >> extract_data >> save_to_json_task >> trigger_update_db
+
+# DAG 2: update_db
+with DAG(
+    dag_id="update_db",
+    default_args=default_args,
+    description="DAG to process JSON file and insert data into both staging and core schemas",
+    catchup=False,
+    schedule=None,
+) as dag_update:
+
+    # Define tasks
+    update_staging = staging_table()
+    update_core = core_table()
+
+
+    # Define dependencies
+    update_staging >> update_core 
